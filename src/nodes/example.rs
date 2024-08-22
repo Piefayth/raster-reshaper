caf3c2881c69b487015276844f4477f10dc320e8
Sean.C.Ryan <Sean.C.Ryan@target.com>
2024-08-22 14:48:31 -0400

    fuck

--- src/nodes/example.rs
+++ src/nodes/example.rs
@@ -24,7 +24,316 @@ use bevy::{
 
 use crate::{setup::{CustomGpuDevice, CustomGpuQueue}, NodeData, NodeKind};
 
-use super::{shared::{Vertex, U32_SIZE}, EdgeDataType, ExampleNodeInputs, ExampleNodeOutputs};
+use super::{macros::declare_node, shared::{Vertex, U32_SIZE}, EdgeDataType, ExampleNodeInputs, ExampleNodeOutputs, Field};
+
+
+declare_node!(
+    name: NewExampleNode,
+    fields: {
+        #[entity] entity: Entity,
+        #[input] texture_extents: Extent3d,
+        #[input] texture_format: TextureFormat,
+        #[input] triangle_color: Vec4,
+        #[output] output_image: Image,
+        render_pipeline: Box<RenderPipeline>,
+        texture_view: Box<TextureView>,
+        bind_group: BindGroup, // todo: just one?
+        texture: Texture,
+        output_buffer: Buffer,
+        vertex_buffer: Buffer,
+        index_buffer: Buffer,
+        color_buffer: Buffer,
+        num_vertices: u32,
+    },
+    methods: {
+        new(
+            entity: Entity,
+            render_device: &CustomGpuDevice, 
+            fragment_source: &Cow<'static, str>,
+            vert_source: &Cow<'static, str>,
+            texture_size: u32,
+            texture_format: TextureFormat,
+        ) -> Self {
+            let frag_shader_module = render_device.create_shader_module(ShaderModuleDescriptor {
+                label: Some("Default Frag Shader Module?"),
+                source: ShaderSource::Wgsl(Cow::Borrowed(fragment_source)),
+            });
+    
+            let vert_shader_module = render_device.create_shader_module(ShaderModuleDescriptor {
+                label: Some("Default Vert Shader Module?"),
+                source: ShaderSource::Wgsl(Cow::Borrowed(vert_source)),
+            });
+    
+            let vertices = &[
+                Vertex {
+                    position: [0.0, 0.5, 0.0],
+                    color: [1.0, 0.0, 0.0],
+                },
+                Vertex {
+                    position: [-0.5, -0.5, 0.0],
+                    color: [0.0, 1.0, 0.0],
+                },
+                Vertex {
+                    position: [0.5, -0.5, 0.0],
+                    color: [0.0, 0.0, 1.0],
+                },
+            ];
+    
+            let indices = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+    
+            let vertex_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(vertices),
+                usage: BufferUsages::VERTEX,
+            });
+    
+            let index_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(indices),
+                usage: BufferUsages::INDEX,
+            });
+    
+            let vertex_buffer_layout = RawVertexBufferLayout {
+                array_stride: std::mem::size_of::<Vertex>() as BufferAddress,
+                step_mode: VertexStepMode::Vertex,
+                attributes: &[
+                    VertexAttribute {
+                        offset: 0,
+                        shader_location: 0,
+                        format: VertexFormat::Float32x3,
+                    },
+                    VertexAttribute {
+                        offset: std::mem::size_of::<[f32; 3]>() as BufferAddress,
+                        shader_location: 1,
+                        format: VertexFormat::Float32x3,
+                    },
+                ],
+            };
+    
+            let color_bind_group_layout = render_device.create_bind_group_layout(
+                "color bind group layout",
+                &[BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+            );
+    
+            let color_data: [f32; 4] = [0.0, 1.0, 1.0, 1.0];
+            let color_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+                label: Some("Color Buffer"),
+                contents: bytemuck::cast_slice(&color_data),
+                usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+            });
+    
+            let color_bind_group = render_device.create_bind_group(
+                "color bind group",
+                &color_bind_group_layout,
+                &[BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::Buffer(BufferBinding {
+                        buffer: &color_buffer,
+                        offset: 0,
+                        size: None,
+                    }),
+                }],
+            );
+    
+            let pipeline_layout = render_device.create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: Some("Pipeline Layout"),
+                bind_group_layouts: &[&color_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+    
+            let texture_extents = Extent3d {
+                width: texture_size,
+                height: texture_size,
+                depth_or_array_layers: 1,
+            };
+    
+            let texture = render_device.create_texture(&TextureDescriptor {
+                label: Some("Texture Name Or Something?"),
+                size: texture_extents,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: TextureDimension::D2,
+                format: texture_format,
+                usage: TextureUsages::STORAGE_BINDING
+                    | TextureUsages::COPY_SRC
+                    | TextureUsages::RENDER_ATTACHMENT,
+                view_formats: &[],
+            });
+    
+            let texture_view = texture.create_view(&Default::default());
+    
+            let output_buffer_size = (U32_SIZE * texture_size * texture_size) as BufferAddress;
+            let output_buffer = render_device.create_buffer(&BufferDescriptor {
+                size: output_buffer_size,
+                usage: BufferUsages::COPY_DST | BufferUsages::MAP_READ,
+                label: None,
+                mapped_at_creation: false,
+            });
+    
+            let render_pipeline = render_device.create_render_pipeline(&RawRenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&pipeline_layout),
+                vertex: RawVertexState {
+                    module: &vert_shader_module,
+                    entry_point: "vertex",
+                    buffers: &[vertex_buffer_layout],
+                    compilation_options: PipelineCompilationOptions::default(),
+                },
+                fragment: Some(RawFragmentState {
+                    module: &frag_shader_module,
+                    entry_point: "fragment",
+                    targets: &[Some(ColorTargetState {
+                        format: texture_format,
+                        blend: Some(BlendState::REPLACE),
+                        write_mask: ColorWrites::ALL,
+                    })],
+                    compilation_options: PipelineCompilationOptions::default(),
+                }),
+                primitive: PrimitiveState {
+                    topology: bevy::render::mesh::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: FrontFace::Ccw,
+                    cull_mode: Some(Face::Back),
+                    polygon_mode: bevy::render::render_resource::PolygonMode::Fill,
+                    unclipped_depth: false, // ????
+                    conservative: false,     // maybe? only on vulkan
+                },
+                depth_stencil: None,
+                multisample: MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
+
+            Self {
+                render_pipeline: Box::new(render_pipeline),
+                texture_view: Box::new(texture_view),
+                texture,
+                vertex_buffer,
+                index_buffer,
+                num_vertices: vertices.len() as u32,
+                output_buffer,
+                color_buffer,
+                bind_group: color_bind_group,
+                texture_extents: Field::Extent3d(texture_extents),
+                texture_format: Field::TextureFormat(texture_format),
+                output_image: Field::Image(None),
+                triangle_color: Field::Vec4(Vec4::ONE),
+                entity,
+            }
+        }
+        process(&mut self, render_device: &CustomGpuDevice, render_queue: &CustomGpuQueue) {
+            let mut encoder = render_device.create_command_encoder(&CommandEncoderDescriptor {
+                label: Some("Command Encoder Descriptor"),
+            });
+    
+            if let Field::Vec4(new_color) = self.triangle_color {
+                render_queue.write_buffer(&self.color_buffer, 0, bytemuck::cast_slice(&[new_color.x, new_color.y, new_color.z, new_color.w]));
+            }
+    
+            {
+                let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[Some(RenderPassColorAttachment {
+                        view: &self.texture_view,
+                        resolve_target: None,
+                        ops: Operations {
+                            load: LoadOp::Clear(LinearRgba::rgb(0.1, 0.2, 0.3).into()),
+                            store: StoreOp::Store,
+                        },
+                    })],
+                    depth_stencil_attachment: None,
+                    timestamp_writes: None,
+                    occlusion_query_set: None,
+                });
+    
+                render_pass.set_pipeline(&self.render_pipeline);
+                render_pass.set_vertex_buffer(0, *self.vertex_buffer.slice(..));
+                render_pass.set_index_buffer(*self.index_buffer.slice(..), IndexFormat::Uint16);
+                render_pass.set_bind_group(0, &self.bind_group, &[]);
+                render_pass.draw(0..self.num_vertices, 0..1);
+            }
+    
+            let input_extents = match self.texture_extents {
+                Field::Extent3d(xtnt) => xtnt,
+                _ => panic!("Silly developer forgot to uphold this invariant. [input_extents"),
+            };
+    
+            let input_texture_format = match self.texture_format
+            {
+                Field::TextureFormat(tfmt) => tfmt,
+                _ => panic!("Silly developer forgot to uphold this invariant. [input_texture_format]"),
+            };
+    
+            encoder.copy_texture_to_buffer(
+                ImageCopyTextureBase {
+                    aspect: TextureAspect::All,
+                    texture: &self.texture,
+                    mip_level: 0,
+                    origin: Origin3d::ZERO,
+                },
+                ImageCopyBuffer {
+                    buffer: &self.output_buffer,
+                    layout: ImageDataLayout {
+                        offset: 0,
+                        bytes_per_row: Some(U32_SIZE * input_extents.width), // todo: width prob wrong here - what happens if aspect ratio != 1? or does aspect ratio HAVE to be padded to 1?
+                        rows_per_image: Some(input_extents.width), // todo: width prob wrong here too
+                    },
+                },
+                input_extents.clone(),
+            );
+    
+            render_queue.submit(Some(encoder.finish()));
+    
+            let image = {
+                let buffer_slice = &self.output_buffer.slice(..);
+    
+                let (s, r) = crossbeam_channel::unbounded::<()>();
+    
+                buffer_slice.map_async(MapMode::Read, move |r| match r {
+                    Ok(_) => {
+                        s.send(()).expect("Failed to send map update");
+                    }
+                    Err(err) => panic!("Failed to map buffer {err}"),
+                });
+    
+                // TODO: We have to figure out how to make this yield instead of just blocking here
+                // Otherwise the task is not cancellable
+                // BUT
+                // in the event that we cancel...
+                // ... we need some way to unmap the buffer I think.
+                render_device.poll(Maintain::wait()).panic_on_timeout();
+    
+                r.recv().expect("Failed to receive map_async message");
+    
+                let buffer: &[u8] = &buffer_slice.get_mapped_range();
+    
+                Image::new_fill(
+                    input_extents.clone(),
+                    TextureDimension::D2,
+                    buffer,
+                    input_texture_format.clone(),
+                    RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+                )
+            };
+    
+            self.output_buffer.unmap();
+            self.output_image = Field::Image(Some(image));
+        }
+    }
+);
+
 
 #[derive(Debug, Clone)]
 pub struct ExampleNode {
--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -6,21 +6,281 @@ use std::fmt::{self, Display};
 
 use bevy::{prelude::*, render::render_resource::{Extent3d, TextureFormat}};
 use color::ColorNode;
-use example::{ExampleNode};
+use example::{ExampleNode, NewExampleNode};
 use petgraph::graph::NodeIndex;
 use subenum::subenum;
 
+use crate::setup::{CustomGpuDevice, CustomGpuQueue};
 
 #[derive(Component)]
 pub struct NodeDisplay {
     pub index: NodeIndex,
 }
 
+
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct InputId(&'static str, &'static str);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct OutputId(&'static str, &'static str);
+
+pub struct Edge {
+    pub from_field: OutputId,
+    pub to_field: InputId,
+}
+
+#[derive(Clone, Debug)]
+pub enum Field {
+    U32(u32),
+    F32(f32),
+    Vec4(Vec4),
+    Extent3d(Extent3d),
+    TextureFormat(TextureFormat),
+    Image(Option<Image>)
+}
+
+impl Field {
+    fn coerce_to(&self, target_type: &Field) -> Result<Field, String> {
+        match (self, target_type) {
+            (Field::U32(v), Field::F32(_)) => Ok(Field::F32(*v as f32)),
+            (Field::U32(v), Field::Vec4(_)) => Ok(Field::Vec4(Vec4::splat(*v as f32))),
+            (Field::F32(v), Field::U32(_)) => Ok(Field::U32(*v as u32)),
+            (Field::F32(v), Field::Vec4(_)) => Ok(Field::Vec4(Vec4::splat(*v))),
+            _ if std::mem::discriminant(self) == std::mem::discriminant(target_type) => {
+                Ok(self.clone())
+            }
+            _ => Err(format!("Cannot coerce {:?} to {:?}", self, target_type)),
+        }
+    }
+}
+
+
+pub trait NodeTrait {
+    fn get_input(&self, id: InputId) -> Option<&Field>;
+    fn get_output(&self, id: OutputId) -> Option<&Field>;
+    fn set_input(&mut self, id: InputId, value: Field) -> Result<(), String>;
+    fn set_output(&mut self, id: OutputId, value: Field) -> Result<(), String>;
+    fn input_type(&self, id: InputId) -> Option<&Field>;
+    fn output_type(&self, id: OutputId) -> Option<&Field>;
+    fn input_fields(&self) -> &[InputId];
+    fn output_fields(&self) -> &[OutputId];
+    fn process(&mut self, render_device: &CustomGpuDevice, render_queue: &CustomGpuQueue);
+    fn entity(&self) -> Entity;
+}
+
+mod macros {
+    macro_rules! declare_node {
+        (
+            name: $node_name:ident,
+            fields: {
+                #[entity] $entity_field:ident: Entity,
+                $(#[input] $input_field:ident: $input_type:ty,)*
+                $(#[output] $output_field:ident: $output_type:ty,)*
+                $($regular_field:ident: $regular_type:ty,)*
+            },
+            methods: {
+                new(
+                    $($param_name:ident: $param_type:ty),* $(,)?
+                ) -> Self $constructor_body:block
+                process($($process_args:tt)*) $process_body:block
+            }
+        ) => {
+            pub struct $node_name {
+                pub $entity_field: Entity,
+                $(pub $input_field: Field,)*
+                $(pub $output_field: Field,)*
+                $(pub $regular_field: $regular_type,)*
+            }
+    
+            impl $node_name {
+                pub fn new($($param_name: $param_type),*) -> Self $constructor_body
+    
+                $(pub const $input_field: $crate::nodes::InputId = $crate::nodes::InputId(stringify!($node_name), stringify!($input_field));)*
+                $(pub const $output_field: $crate::nodes::OutputId = $crate::nodes::OutputId(stringify!($node_name), stringify!($output_field));)*
+            }
+    
+            impl $crate::nodes::NodeTrait for $node_name {
+                fn get_input(&self, id: $crate::nodes::InputId) -> Option<&Field> {
+                    match id {
+                        $(Self::$input_field => Some(&self.$input_field),)*
+                        _ => None,
+                    }
+                }
+    
+                fn get_output(&self, id: $crate::nodes::OutputId) -> Option<&Field> {
+                    match id {
+                        $(Self::$output_field => Some(&self.$output_field),)*
+                        _ => None,
+                    }
+                }
+    
+                fn set_input(&mut self, id: $crate::nodes::InputId, value: $crate::nodes::Field) -> Result<(), String> {
+                    match id {
+                        $(Self::$input_field => {
+                            self.$input_field = value.coerce_to(&self.$input_field)?;
+                            Ok(())
+                        })*
+                        _ => Err(format!("Invalid input field ID for {}", stringify!($node_name))),
+                    }
+                }
+    
+                fn set_output(&mut self, id: $crate::nodes::OutputId, value: Field) -> Result<(), String> {
+                    match id {
+                        $(Self::$output_field => {
+                            self.$output_field = value.coerce_to(&self.$output_field)?;
+                            Ok(())
+                        })*
+                        _ => Err(format!("Invalid output field ID for {}", stringify!($node_name))),
+                    }
+                }
+    
+                fn input_type(&self, id: $crate::nodes::InputId) -> Option<&Field> {
+                    self.get_input(id)
+                }
+    
+                fn output_type(&self, id: $crate::nodes::OutputId) -> Option<&Field> {
+                    self.get_output(id)
+                }
+    
+                fn input_fields(&self) -> &[$crate::nodes::InputId] {
+                    &[$(Self::$input_field,)*]
+                }
+    
+                fn output_fields(&self) -> &[$crate::nodes::OutputId] {
+                    &[$(Self::$output_field,)*]
+                }
+    
+                fn process($($process_args)*) $process_body
+    
+                fn entity(&self) -> Entity {
+                    self.$entity_field
+                }
+            }
+        }
+    }
+
+    pub(crate) use declare_node; 
+}
+
+
+macro_rules! declare_node_enum_and_impl_trait {
+    (
+        $(#[$meta:meta])*
+        $vis:vis enum $enum_name:ident {
+            $($variant:ident($node_type:ty)),* $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        $vis enum $enum_name {
+            $($variant($node_type)),*
+        }
+
+        impl NodeTrait for $enum_name {
+            fn get_input(&self, id: InputId) -> Option<&Field> {
+                match self {
+                    $($enum_name::$variant(n) => n.get_input(id),)*
+                }
+            }
+
+            fn get_output(&self, id: OutputId) -> Option<&Field> {
+                match self {
+                    $($enum_name::$variant(n) => n.get_output(id),)*
+                }
+            }
+
+            fn set_input(&mut self, id: InputId, value: Field) -> Result<(), String> {
+                match self {
+                    $($enum_name::$variant(n) => n.set_input(id, value),)*
+                }
+            }
+
+            fn set_output(&mut self, id: OutputId, value: Field) -> Result<(), String> {
+                match self {
+                    $($enum_name::$variant(n) => n.set_output(id, value),)*
+                }
+            }
+
+            fn input_type(&self, id: InputId) -> Option<&Field> {
+                match self {
+                    $($enum_name::$variant(n) => n.input_type(id),)*
+                }
+            }
+
+            fn output_type(&self, id: OutputId) -> Option<&Field> {
+                match self {
+                    $($enum_name::$variant(n) => n.output_type(id),)*
+                }
+            }
+
+            fn input_fields(&self) -> &[InputId] {
+                match self {
+                    $($enum_name::$variant(n) => n.input_fields(),)*
+                }
+            }
+
+            fn output_fields(&self) -> &[OutputId] {
+                match self {
+                    $($enum_name::$variant(n) => n.output_fields(),)*
+                }
+            }
+
+            fn process(&mut self, render_device: &CustomGpuDevice, render_queue: &CustomGpuQueue) {
+                match self {
+                    $($enum_name::$variant(n) => n.process(render_device, render_queue),)*
+                }
+            }
+
+            fn entity(&self) -> Entity {
+                match self {
+                    $($enum_name::$variant(n) => n.entity(),)*
+                }
+            }
+        }
+    }
+}
+
+declare_node_enum_and_impl_trait! {
+    pub enum Node {
+        NewExampleNode(NewExampleNode),
+        // other node kinds
+    }
+}
+
+// declare_node!(
+//     name: ConstantNode,
+//     fields: {
+//         #[output] out_number: u32,
+//     },
+//     methods: {
+//         new(value: u32) -> Self {
+//             Self {
+//                 out_number: Field::U32(value),
+//                 input_ids: vec![],
+//                 output_ids: vec![Self::out_number],
+//             }
+//         }
+//         process(this) {
+            
+//         }
+//     }
+// );
+
+
+
+
+
+
+// ---- OLD CODE BELOW ----
+
+
+
 #[derive(Debug, Clone)]
 pub enum NodeKind {
     Example(ExampleNode),
     Color(ColorNode)
 }
+
 
 impl Display for NodeKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
